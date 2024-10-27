3e1c4eb4744efab12b03c57126e76094bb383966
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-26 22:59:32 -0300

    :lipstick: style: tag select category

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { CheckIcon, EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 
 interface SelectCategoryProps {
   defaultCategory: string,
@@ -12,14 +12,14 @@ export default function SelectCategory({defaultCategory, categories}: SelectCate
 
   return (
     <Listbox value={selected} onChange={setSelected}>
-      <div className="relative mt-2">
-        <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+      <div className="relative">
+        <ListboxButton className="w-full inline-flex items-center gap-x-0.5 mr-0.5 rounded-md bg-gray-50 px-2 py-2 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
           <span className="flex items-center">
 {/*             <img alt="" src={selected.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
-            <span className="ml-3 block truncate">{selected}</span>
+            <span className="mx-3 block truncate">{selected}</span>
           </span>
           <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-            <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+            <EllipsisVerticalIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
           </span>
         </ListboxButton>
 
@@ -35,7 +35,7 @@ export default function SelectCategory({defaultCategory, categories}: SelectCate
             >
               <div className="flex items-center">
 {/*                 <img alt="" src={person.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
-                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                <span className="block truncate font-normal group-data-[selected]:font-semibold">
                   {category}
                 </span>
               </div>
