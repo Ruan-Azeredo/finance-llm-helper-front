b5b8153c4ee8e6439a513539a888ba2887cdfea5
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-29 00:00:44 -0300

    :construction: build: default categories const

---
+++ src/default_categories.ts
@@ -0,0 +1,13 @@
+export const default_categories = [
+    "Alimentação",
+    "Receitas",
+    "Saúde",
+    "Transporte",
+    "Mercado", 
+    "Educação",
+    "Compras",
+    "Investimento",
+    "Transferencia para terceiros", 
+    "Telefone",
+    "Moradia"
+]
