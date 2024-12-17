d9c316c7b1535f5a8cb2c22cebcf65d51e2c64dc
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-17 09:38:19 -0300

    :construction: build: add my photo temporarily

--- src/LayoutContainer.jsx
+++ src/LayoutContainer.jsx
@@ -20,7 +20,7 @@ export default function LayoutContainer({ children, categories, chart }) {
                 <span className="sr-only">Your profile</span>
                 <img
                   alt=""
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  src="https://github.com/Ruan-Azeredo.png"
                   className="h-8 w-8 rounded-full bg-gray-800"
                 />
               </a>
