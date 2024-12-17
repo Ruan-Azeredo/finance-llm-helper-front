88ffdaca82b1b6f19ea77b7a0b7ff1fb6a5612b1
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-17 09:39:09 -0300

    :construction: build: create table with ops for transactions

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -1,8 +1,12 @@
+import { ArrowUturnLeftIcon, XCircleIcon, XMarkIcon } from "@heroicons/react/24/outline"
 import { default_categories } from "../default_categories"
 import SelectCategory from "./SelectCategory"
+import { useState } from "react"
 
-export  default function TransactionsTable({transactions}) {
+export default function TransactionsTable({transactions}) {
     console.log('transactions: ', transactions)
+    const preDeleted = []
+    const [t, setT] = useState(transactions)
     return (
         <div className="px-4 sm:px-6 lg:px-8">
 {/*             <div className="sm:flex sm:items-center">
@@ -27,34 +31,75 @@ export  default function TransactionsTable({transactions}) {
                     </th>
                     </tr> */}
                 </thead>
-                {transactions?.map((transaction) => (
-                    <tbody key={transaction.id} className="divide-y divide-gray-200 bg-white">
+                {t?.map((transaction) => (
+                    <tbody className={`divide-y divide-gray-200 bg-white`}>
                         <tr>
-                        <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                            <dl className="font-normal lg:hidden">
-                                <dt className="sr-only">Data</dt>
-                                <dd className="mt-1 truncate text-gray-700">{transaction.date}</dd>
-                                <dt className="sr-only">Descrição</dt>
-                                <dd className="mt-1 truncate text-gray-700">{transaction.memo}</dd>
-                                <dt className="sr-only sm:hidden">Email</dt>
-                                <dd className="mt-1 truncate text-gray-500 sm:hidden">{transaction.amount}</dd>
-                            </dl>
-                        </td>
-                        <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{transaction.date}</td>
-                        <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{transaction.memo}</td>
-                        <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{transaction.amount}</td>
-                        {/* <td className="px-3 py-4 text-sm text-gray-500">{transaction.category}</td> */}
-                        <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <SelectCategory defaultCategory={transaction.category} categories={default_categories}/>
-                            {/* <a href="#" className="text-indigo-600 hover:text-indigo-900">{transaction.category}<span className="sr-only">, Lindsay Walton</span></a> */}
-                        </td>
+                            <td className={`${transaction.id} w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0`}>
+                                <dl className="font-normal lg:hidden">
+                                    <dt className="sr-only">Data</dt>
+                                    <dd className="mt-1 truncate text-gray-700">{transaction.date}</dd>
+                                    <dt className="sr-only">Descrição</dt>
+                                    <dd className="mt-1 truncate text-gray-700">{transaction.memo}</dd>
+                                    <dt className="sr-only sm:hidden">Email</dt>
+                                    <dd className="mt-1 truncate text-gray-500 sm:hidden">{transaction.amount}</dd>
+                                    <dt className="sr-only sm:hidden">Delete</dt>
+                                    <dd><XCircleIcon/></dd>
+                                </dl>
+                            </td>
+                            <td className={`${transaction.id} hidden px-3 py-4 text-sm text-gray-500 lg:table-cell`}>{transaction.date}</td>
+                            <td className={`${transaction.id} hidden px-3 py-4 text-sm text-gray-500 lg:table-cell`}>{transaction.memo}</td>
+                            <td className={`${transaction.id} hidden px-3 py-4 text-sm text-gray-500 sm:table-cell`}>{transaction.amount}</td>
+                            {/* <td className="px-3 py-4 text-sm text-gray-500">{transaction.category}</td> */}
+                            <td className={`${transaction.id} py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0`}>
+                                <SelectCategory defaultCategory={transaction.category} categories={default_categories}/>
+                                {/* <a href="#" className="text-indigo-600 hover:text-indigo-900">{transaction.category}<span className="sr-only">, Lindsay Walton</span></a> */}
+                            </td>
+                            <td>
+                                <button id={transaction.id + 'x-btn'} type="button" onClick={() => {
+                                        /* preDeleted.push(transaction.id)
+                                        console.log('preDeleted: ', preDeleted) */
+                                        const items = document.getElementsByClassName(`${transaction.id}`)
+                                        console.log('items: ', items)
+                                        Array.from(items).forEach(element => {
+                                            element.classList.add('blur-sm');
+                                        });
+                                        document.getElementById(transaction.id + 'rm-btn').classList.remove('hidden')
+                                        document.getElementById(transaction.id + 'x-btn').classList.add('hidden')
+                                        document.getElementById(transaction.id + 'back-btn').classList.remove('hidden')
+                                    }}>
+                                    <XMarkIcon className="text-gray-500 ml-4 h-4 w-4"/>
+                                </button>
+                                <button id={transaction.id + 'back-btn'} className="hidden" type="button" onClick={() => {
+                                        /* preDeleted.pop(transaction.id)
+                                        console.log('preDeleted: ', preDeleted) */
+                                        const items = document.getElementsByClassName(`${transaction.id}`)
+                                        Array.from(items).forEach(element => {
+                                            element.classList.remove('blur-sm');
+                                        });
+                                        document.getElementById(transaction.id + 'rm-btn').classList.add('hidden')
+                                        document.getElementById(transaction.id + 'x-btn').classList.remove('hidden')
+                                        document.getElementById(transaction.id + 'back-btn').classList.add('hidden')
+                                    }}>
+                                    <ArrowUturnLeftIcon className="text-gray-500 ml-4 h-4 w-4"/>
+                                </button>
+                            </td>
                         </tr>
+                        <div className=" w-fit">
+                            <button onClick={() => {
+                                setT(t.filter((transact) => transact.id !== transaction.id))
+                                document.getElementById(transaction.id + 'rm-btn').classList.add('hidden')
+                                document.getElementById(transaction.id + 'x-btn').classList.remove('hidden')
+                                document.getElementById(transaction.id + 'back-btn').classList.add('hidden')
 
+                                console.log('t: ', t)
+                            }} id={transaction.id + 'rm-btn'} type="button" class="-mt-[54px] absolute hidden justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Remover</button>
+                        </div>
                     </tbody>
 
                 ))}
                 </table>
             </div>
+            <div onClick={() => console.log('t: ', t)}>kjsdfs</div>
         </div>
 
     )
