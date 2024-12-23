91c729860900b5c60ad1751eb357ce7217ba76cf
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-23 11:58:45 -0300

    :recycle: refactor: layout container

--- src/LayoutContainer.jsx
+++ src/LayoutContainer.jsx
@@ -1,48 +1,223 @@
-import { BellIcon } from '@heroicons/react/24/outline'
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Popover,
+  PopoverBackdrop,
+  PopoverButton,
+  PopoverPanel,
+} from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import AddTransactions from './pages/AddTransactions'
 
-export default function LayoutContainer({ children, categories, chart }) {
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://api.dicebear.com/9.x/micah/svg?seed=Eden&baseColor=f9c9b6,ac6651&mouth=laughing,pucker,smile,smirk,surprised&backgroundColor=b6e3f4',
+}
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'Profile', href: '#', current: false },
+  { name: 'Resources', href: '#', current: false },
+  { name: 'Company Directory', href: '#', current: false },
+  { name: 'Openings', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+
+export default function LayoutContainer() {
   return (
     <>
-      <div className="flex min-h-full flex-col">
-        <header className="shrink-0 bg-gray-900">
-          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-            <img
-              alt="Your Company"
-              src="src/assets/spendlyzer-dark-logo.png"
-              className="h-10 w-auto"
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex flex-col min-h-screen bg-gradient-to-tr from-slate-200 to-slate-50">
+        <Popover as="header" className="pb-24 mx-6 rounded-b-xl bg-gradient-to-br from-slate-900 to-slate-700">
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="relative flex items-center justify-center py-5 lg:justify-between">
+              {/* Logo */}
+              <div className="absolute left-0 flex-shrink-0 lg:static">
+                <a href="#">
+                  <span className="sr-only">Your Company</span>
+                  <img
+                    alt="Your Company"
+                    src="src/assets/spendlyzer-full-logo.png"
+                    className="h-12 w-auto"
+                  />
+                </a>
+              </div>
+
+              {/* Right section on desktop */}
+              <div className="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
+                <button
+                  type="button"
+                  className="relative flex-shrink-0 rounded-full p-1 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon aria-hidden="true" className="h-6 w-6" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-4 flex-shrink-0">
+                  <div>
+                    <MenuButton className="relative flex rounded-full bg-white text-sm ring-2 ring-white ring-opacity-20 focus:outline-none focus:ring-opacity-100">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img alt="" src={user.imageUrl} className="h-8 w-8 rounded-full" />
+                    </MenuButton>
+                  </div>
+                  <MenuItems
+                    transition
+                    className="absolute -right-2 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:scale-95 data-[closed]:data-[leave]:transform data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-75 data-[leave]:ease-in"
+                  >
+                    {userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        <a href={item.href} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                          {item.name}
+                        </a>
+                      </MenuItem>
+                    ))}
+                  </MenuItems>
+                </Menu>
+              </div>
+              
+
+              {/* Menu button */}
+              <div className="absolute right-0 flex-shrink-0 lg:hidden">
+                {/* Mobile menu button */}
+                <PopoverButton className="group relative inline-flex items-center justify-center rounded-md bg-transparent p-2 text-indigo-200 hover:bg-white hover:bg-opacity-10 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+                  <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                </PopoverButton>
+              </div>
+            </div>
+            
+          </div>
+
+          <div className="lg:hidden">
+            <PopoverBackdrop
+              transition
+              className="fixed inset-0 z-20 bg-black bg-opacity-25 duration-150 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in"
             />
-            <div className="flex items-center gap-x-8">
-              <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
-                <span className="sr-only">View notifications</span>
-                <BellIcon ariaHidden="true" className="h-6 w-6" />
-              </button>
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your profile</span>
-                <img
-                  alt=""
-                  src="https://github.com/Ruan-Azeredo.png"
-                  className="h-8 w-8 rounded-full bg-gray-800"
-                />
-              </a>
+
+            <PopoverPanel
+              focus
+              transition
+              className="absolute inset-x-0 top-0 z-30 mx-auto w-full max-w-3xl origin-top transform p-2 transition duration-150 data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="divide-y divide-gray-200 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="pb-2 pt-3">
+                  <div className="flex items-center justify-between px-4">
+                    <div>
+                      <img
+                        alt="Your Company"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        className="h-8 w-auto"
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <PopoverButton className="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="absolute -inset-0.5" />
+                        <span className="sr-only">Close menu</span>
+                        <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                      </PopoverButton>
+                    </div>
+                  </div>
+                  <div className="mt-3 space-y-1 px-2">
+                    <a
+                      href="#"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Home
+                    </a>
+                    <a
+                      href="#"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Profile
+                    </a>
+                    <a
+                      href="#"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Resources
+                    </a>
+                    <a
+                      href="#"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Company Directory
+                    </a>
+                    <a
+                      href="#"
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Openings
+                    </a>
+                  </div>
+                </div>
+                <div className="pb-2 pt-4">
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img alt="" src={user.imageUrl} className="h-10 w-10 rounded-full" />
+                    </div>
+                    <div className="ml-3 min-w-0 flex-1">
+                      <div className="truncate text-base font-medium text-gray-800">{user.name}</div>
+                      <div className="truncate text-sm font-medium text-gray-500">{user.email}</div>
+                    </div>
+                    <button
+                      type="button"
+                      className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon aria-hidden="true" className="h-6 w-6" />
+                    </button>
+                  </div>
+                  <div className="mt-3 space-y-1 px-2">
+                    {userNavigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </PopoverPanel>
+          </div>
+        </Popover>
+        <main className="flex-grow -mt-24 pb-8">
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            
+            <AddTransactions/>
+
+          </div>
+        </main>
+        <footer className='relative '>
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+              <span className="block sm:inline">&copy; 2021 Your Company, Inc.</span>{' '}
+              <span className="block sm:inline">All rights reserved.</span>
             </div>
           </div>
-        </header>
-
-        {/* 3 column wrapper */}
-        <div className="mx-auto w-full max-w-7xl grow lg:flex xl:px-2">
-          {/* Left sidebar & main wrapper */}
-          <div className="flex-1 xl:flex">
-            <div className="border-b border-gray-200 px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-6">
-              {categories}
-            </div>
-
-            <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">{children}</div>
-          </div>
-
-          <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6">
-            {chart}
-          </div>
-        </div>
+        </footer>
       </div>
     </>
   )
