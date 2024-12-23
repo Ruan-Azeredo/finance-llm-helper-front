61af1013322d123faaa916318289c4413b3d7e1a
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-23 12:02:56 -0300

    :recycle: refactor: turn sections component

---
+++ src/components/SectionContainer.tsx
@@ -0,0 +1,12 @@
+export default function SectionContainer({ children, title }: { children: React.ReactNode, title: string}) {
+    return (
+        <section aria-labelledby={title}>
+            <h2 id={title} className="sr-only">
+            {title}
+            </h2>
+            <div className="overflow-hidden rounded-lg bg-white shadow">
+            {children}
+            </div>
+        </section>
+    )
+}
---
+++ src/components/sections/TransactionsSection.tsx
@@ -0,0 +1,16 @@
+import { StyledButton } from "../micro/StyledButton"
+import TransactionsTable from "../TransactionsTable"
+import { Transaction } from "../../schemas/Transaction"
+
+export default function TransactionsSection({ transactions }: { transactions: Transaction[]}) {
+    return (
+        <div className="p-6 flex flex-col">
+            <div className='flex justify-between'>
+            <div className=' text-lg/7 font-semibold text-gray-900'>Transações</div>
+            <StyledButton.Root className='ml-auto mr-4' type='secondary'>Saldo Total: R$ 25,54</StyledButton.Root>
+            <StyledButton.Root type='dark'>Salvar</StyledButton.Root>
+            </div>
+            <TransactionsTable transactions={transactions}/>
+        </div>
+    )
+}
