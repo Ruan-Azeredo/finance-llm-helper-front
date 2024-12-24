603f9ceb72596a3495df537212a7e55ec8a896c4
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-24 11:27:14 -0300

    :construction: build: add invalid state to input

--- src/components/micro/InputText/Root.tsx
+++ src/components/micro/InputText/Root.tsx
@@ -1,20 +1,24 @@
+import { ExclamationCircleIcon } from "@heroicons/react/24/solid"
+
 interface InputTextProps {
-  label: string,
+  label: string
   children?: React.ReactNode
   className?: string
   placeholder?: string
+  validationError?: boolean
+  invalidMessage?: string
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 
-export default function Root({label, children, className, placeholder, onChange}: InputTextProps) {
+export default function Root({label, children, className, placeholder, validationError, invalidMessage, onChange}: InputTextProps) {
     return (
       <div className={className}>
         <label htmlFor={label} className="block text-sm font-medium leading-6 text-gray-900">
           {label}
         </label>
         <div className={`mt-2`}>
-          <div className="flex pl-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+          <div className={`flex pl-2 rounded-md shadow-sm ring-1 ring-inset ${validationError ? 'ring-red-300' : 'ring-gray-300'} focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600`}>
             {children}
             <input
               id={label}
@@ -22,10 +26,29 @@ export default function Root({label, children, className, placeholder, onChange}
               type="text"
               onChange={onChange}
               placeholder={placeholder}
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              aria-invalid={validationError}
+              className={
+                `block flex-1 border-0 bg-transparent py-1.5 pl-1 focus:ring-0 sm:text-sm sm:leading-6 
+                ${
+                  validationError ? 
+                  'text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500' 
+                  :
+                  'text-gray-900 placeholder:text-gray-400'
+                }`
+              }
             />
+            {validationError ? (
+              <div className="pointer-events-none inset-y-0 right-0 flex items-center pr-3">
+                <ExclamationCircleIcon aria-hidden="true" className="h-5 w-5 text-red-500" />
+              </div>
+            ) : null}
           </div>
         </div>
+        {validationError ? (
+          <p id="email-error" className="mt-2 text-sm text-red-600">
+            {invalidMessage}
+          </p>
+        ) : null}
       </div>
     )
   }
