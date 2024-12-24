5703d6e5cd2f82c741f047997cdfa0c5b98e98ad
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-24 11:29:48 -0300

    :fire: chore: remove non used import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import LayoutContainer from './LayoutContainer';
-import InputCategory from './components/InputCategory';
-import BadgeCategory from './components/BadgeCategory';
-import TransactionsTable from './components/TransactionsTable';
-import FileUpload from './components/FileUpload';
-import { default_categories } from './default_categories';
-import AwaitModal from './components/AwaitModal';
-import { StyledButton } from './components/micro/StyledButton';
-import { InputText } from './components/micro/InputText';
-import UploadFileButton from './components/UploadFileButton';
-import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
 
 function App() {
 
