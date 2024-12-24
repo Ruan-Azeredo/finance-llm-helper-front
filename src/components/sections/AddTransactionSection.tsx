1efdfcb33d69678f550358f64b0275bc6f30f61e
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-24 11:25:52 -0300

    :construction: build: validate form

--- src/components/sections/AddTransactionSection.tsx
+++ src/components/sections/AddTransactionSection.tsx
@@ -4,6 +4,7 @@ import { InputText } from "../micro/InputText"
 import { StyledButton } from "../micro/StyledButton"
 import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline"
 import { Transaction } from "../../schemas/Transaction"
+import { isValidAmountFormat, isValidDateFormat } from "../logic/validator"
 
 
 interface AddTransactionSectionProps {
@@ -16,30 +17,55 @@ export default function AddTransactionSection({ transactions, setTransactions, s
 
     const [showTransactionForm, setShowTransactionForm] = useState(false)
 
-    const [incomeOrExpense, setIncomeOrExpense] = useState<null | "income" | "expense">(null)
+    const [incomeOrExpense, setIncomeOrExpense] = useState<"income" | "expense">("income")
     const [transactionDescription, setTransactionDescription] = useState<null | string>(null)
-    const [transactionValue, setTransactionValue] = useState<null | string>(null)
+    const [transactionAmount , setTransactionAmount] = useState<null | string>(null)
+    const [invalidAmount, setInvalidAmount] = useState<boolean>(false)
     const [transactionDate, setTransactionDate] = useState<null | string>(null)
+    const [invalidDate, setInvalidDate] = useState<boolean>(false)
     const [transactionCategory, setTransactionCategory] = useState<null | string>(null)
 
     const addTransaction = () => {
 
-        if(incomeOrExpense === "expense"){
-            setTransactionValue(`-${transactionValue}`)
+        let editedTransactionAmount: string = ''
+        if(incomeOrExpense === "expense"){ 
+            editedTransactionAmount = `-${transactionAmount}`
         } else if(incomeOrExpense === "income"){
-            setTransactionValue(`${transactionValue}`)
+            editedTransactionAmount = `${transactionAmount}`
         }
 
         const transaction = {
           id: crypto.randomUUID(),
           memo: transactionDescription,
-          amount: transactionValue,
+          amount: editedTransactionAmount,
           date: transactionDate,
           category: transactionCategory,
         }
     
         setTransactions([...transactions, transaction])
-    } 
+    }
+
+    const validateForm = () => {
+
+        if(!isValidDateFormat(transactionDate!)){
+            setInvalidDate(true)
+        } else{
+            setInvalidDate(false)
+        }
+
+        if(!isValidAmountFormat(transactionAmount!)){
+            setInvalidAmount(true)
+        } else{
+            setInvalidAmount(false)
+        }
+
+        if(
+            isValidDateFormat(transactionDate!)
+            && isValidAmountFormat(transactionAmount!)
+        ){
+            addTransaction()
+        }
+    }
 
     return (
         <div className="p-6">
@@ -60,19 +86,40 @@ export default function AddTransactionSection({ transactions, setTransactions, s
                 
                 <div className={showTransactionForm ? '' : 'hidden'} >
                     <div className={`mt-6 flex gap-4`}>
-                        <InputText.Root label='Descrição' className='w-full' onChange={(e) => setTransactionDescription(e.target.value)}/>
-                        <InputText.Root label='Valor' placeholder='0,00' onChange={(e) => setTransactionValue(e.target.value)}>
+                        <InputText.Root
+                            label='Descrição'
+                            className='w-full'
+                            onChange={(e) => setTransactionDescription(e.target.value)}
+                        />
+                        <InputText.Root 
+                            label='Valor'
+                            placeholder='0,00'
+                            onChange={(e) => setTransactionAmount(e.target.value)}
+                            validationError={invalidAmount}
+                            invalidMessage='Utilize o formato 0,00'
+                        >
                             <InputText.AddOn text='R$'/>
                         </InputText.Root>
                     </div>
                     <div className='mt-2 flex gap-4'>
-                        <InputText.Root label='Data' placeholder='dd/mm/aaaa' onChange={(e) => setTransactionDate(e.target.value)}/>
-                        <InputText.Root label='Categoria' className='w-full' onChange={(e) => setTransactionCategory(e.target.value)}/>
-                        <div className='h-9 flex mt-auto'>
+                        <InputText.Root
+                            label='Data'
+                            placeholder='dd/mm/aaaa'
+                            onChange={(e) => setTransactionDate(e.target.value)}
+                            validationError={invalidDate}
+                            invalidMessage='Utilize uma data válida e com o formato DD/MM/AAAA'
+                        />
+                        <InputText.Root
+                            label='Categoria'
+                            className='w-full'
+                            onChange={(e) => setTransactionCategory(e.target.value)}
+                        />
+                        <div className='h-9 flex mt-8'>
                             <span className="isolate inline-flex rounded-md shadow-sm">
                                 <button
                                     onClick={() => setIncomeOrExpense('income')}
                                     type="button"
+                                    defaultChecked
                                     className={`relative -ml-px inline-flex items-center rounded-l-md ${incomeOrExpense === 'income' ? 'bg-green-600 text-white' : 'bg-white text-gray-900 hover:bg-gray-50'} px-3 text-sm font-semibold  ring-1 ring-inset ring-gray-300 focus:z-10`}
                                 >
                                     <span><PlusIcon className={`h-5 w-5 mr-1 ${incomeOrExpense === 'income' ? 'text-white' : 'text-green-500'}`}/></span>Entrada
@@ -86,7 +133,7 @@ export default function AddTransactionSection({ transactions, setTransactions, s
                                 </button>
                             </span>
                         </div>
-                        <StyledButton.Root className='h-9 mt-auto' action={addTransaction}>Salvar</StyledButton.Root>
+                        <StyledButton.Root className='h-9 mt-8' action={validateForm}>Adicionar</StyledButton.Root>
                     </div>
                 </div>
             </div>
