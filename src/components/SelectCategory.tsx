61248f1f50a6ac0c141804c1323c2090eb1d4dc1
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-25 15:48:39 -0300

    :goal_net: chore: update transaction tag correctly

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -1,46 +1,45 @@
-import { useState } from 'react'
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
-import { CheckIcon, EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { CheckIcon } from '@heroicons/react/20/solid'
+import { Transaction } from '../schemas/Transaction'
+import { ChevronUpDownIcon } from '@heroicons/react/24/outline'
 
 interface SelectCategoryProps {
-  defaultCategory: string,
+  transactions: Transaction[]
+  setTransactions: React.Dispatch<React.SetStateAction<Transaction[]>>
+  listIndex: number
   categories: string[]
 }
 
-export default function SelectCategory({defaultCategory, categories}: SelectCategoryProps) {
-  const [selected, setSelected] = useState(defaultCategory)
+export default function SelectCategory({transactions, setTransactions, listIndex, categories}: SelectCategoryProps) {
+  
+  const updateCategory = (value: string) => {
+    transactions[listIndex].category = value
+    setTransactions([...transactions])
+  }
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      <div className="relative">
-        <ListboxButton className="w-full inline-flex items-center gap-x-0.5 mr-0.5 rounded-md bg-gray-50 px-2 py-2 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-          <span className="flex items-center">
-{/*             <img alt="" src={selected.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
-            <span className="mx-3 block truncate">{selected}</span>
-          </span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-            <EllipsisVerticalIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+    <Listbox value={transactions[listIndex]?.category} onChange={updateCategory}>
+      <div className="w-40 relative">
+        <ListboxButton className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <span className="block truncate max-w-28">{transactions[listIndex]?.category}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
           </span>
         </ListboxButton>
 
         <ListboxOptions
           transition
-          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+          className="absolute z-10 mt-1 max-h-40 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
         >
-          {categories?.map((category) => (
+          {categories.map((category) => (
             <ListboxOption
               key={category}
               value={category}
-              className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+              className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
             >
-              <div className="flex items-center">
-{/*                 <img alt="" src={person.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
-                <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                  {category}
-                </span>
-              </div>
+              <span className="block truncate font-normal group-data-[selected]:font-semibold">{category}</span>
 
-              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
                 <CheckIcon aria-hidden="true" className="h-5 w-5" />
               </span>
             </ListboxOption>
