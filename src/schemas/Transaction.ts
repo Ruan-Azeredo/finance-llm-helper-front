a1b204474a23f0164f27d04a19ef65f613a6e884
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-12 18:18:10 -0300

    :construction: build: improve type transaction

--- src/schemas/Transaction.ts
+++ src/schemas/Transaction.ts
@@ -1,7 +1,8 @@
 export interface Transaction {
-    id: string
+    id: string | number | null
     memo: string | null
     amount: string | null
     date: string | null
     category: string | null
+    direction: "income"| "expense" | null
 }
