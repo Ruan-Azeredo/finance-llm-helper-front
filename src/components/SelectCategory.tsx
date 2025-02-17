f57e631bf85405db1c98f9cef24192398ace1742
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-17 10:43:56 -0300

    :goal_net: fix: make update to select in table  when confirm the   changes in modal

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -1,7 +1,7 @@
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 import { CheckIcon } from '@heroicons/react/20/solid'
 import { ChevronUpDownIcon } from '@heroicons/react/24/outline'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { TransactionsTemplateContext } from '../contexts/TransactionsTemplate'
 import { Transaction } from '../schemas/Transaction'
 
@@ -17,7 +17,11 @@ export default function SelectCategory({transaction, setCategory, categories}: S
 
   const { update_transaction } = useContext(TransactionsTemplateContext)
 
-  const [selected, setSelected] = useState(categories[0])
+  const [selected, setSelected] = useState(transaction?.category ? transaction.category : categories[0])
+
+  useEffect(() => {
+    setSelected(transaction?.category ? transaction.category : categories[0])
+  }, [categories, transaction])
 
   const on_change = (value: string) => {
 
@@ -29,15 +33,16 @@ export default function SelectCategory({transaction, setCategory, categories}: S
       new_transaction.category = value
 
       update_transaction(new_transaction)
+      setSelected(value)
     }
 
   }
 
   return (
-    <Listbox value={transaction?.category ? transaction.category : selected} onChange={on_change}>
+    <Listbox value={selected} onChange={on_change}>
       <div className="w-60 relative">
         <ListboxButton className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          <span className="block truncate max-w-28">{transaction?.category ? transaction.category : selected}</span>
+          <span className="block truncate max-w-28">{selected}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
           </span>
