da6a48c2d42553aab0ac5bdf685810b70948ffaf
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-19 15:13:33 -0300

    :lipstick: chore: improve better ui to chart card

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -12,6 +12,8 @@ import { TransactionsTemplateContext } from "../contexts/TransactionsTemplate";
 import { Transaction } from "../schemas/Transaction";
 import { ApexOptions } from "apexcharts";
 import { wrap } from "module";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
    
   // If you're using Next.js please use the dynamic import for react-apexcharts and remove the import from the top for the react-apexcharts
   // import dynamic from "next/dynamic";
@@ -80,8 +82,8 @@ import { wrap } from "module";
     
     const chartConfig = {
 
-      with: 400,
-      height: 340,
+      width: '100%',
+      height: 280,
       dataLabels: {
         enabled: true,
       },
@@ -102,12 +104,12 @@ import { wrap } from "module";
         legend: {
           show: true,
           position: 'bottom',
-          fontSize: '14px',
-          fontFamily: 'Inter, sans-serif',
+          fontSize: '12px',
+          fontFamily: "Geist",
           clusterGroupedSeriesOrientation: 'horizontal',
           //offsetY: 100,
           itemMargin: {
-            horizontal: 12,
+            horizontal: 10,
             vertical: 5,
             
           },
@@ -129,27 +131,44 @@ import { wrap } from "module";
 
     return (
       <Card className="flex justify-center" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+
+        <div className="flex flex-col mb-2">
+          <div className="block text-sm font-medium leading-6 text-gray-900 ">Estatística  das Categoria</div>
         
-          <div className="flex flex-col items-center justify-center gap-4">
+          <Menu as="div" className="relative w-fit inline-block text-left group">
+            <div>
+              <MenuButton className="inline-flex justify-center rounded-md bg-white text-sm font-semibold text-gray-400 shadow-xs group-hover:text-gray-600">
+                {sei ? "Despesas" : "Receitas"}
+                <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-gray-400 group-hover:text-gray-600" />
+              </MenuButton>
+            </div>
+
+            <MenuItems
+              transition
+              className="absolute left-0 z-10 mt-2 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+            >
+              <div className="py-1">
+                <MenuItem>
+                  <button
+                    onClick={() => setSei(true)}
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden  hover:bg-gray-100 hover:text-gray-900 w-full"
+                  >
+                    Despesas
+                  </button>
+                </MenuItem>
+                <MenuItem>
+                  <button
+                    onClick={() => setSei(false)}
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden  hover:bg-gray-100 hover:text-gray-900 w-full"
+                  >
+                    Receitas
+                  </button>
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </Menu>
+        </div>
             <Chart type="donut" {...chartConfig} />
-            <span className="">
-              <button
-                onClick={() => setSei(false)}
-                type="button"
-                defaultChecked
-                className={`relative -ml-px inline-flex items-center rounded-l-md py-2 px-4 ${!sei ? 'bg-slate-600 text-white' : 'bg-white text-gray-900 hover:bg-gray-50'} text-sm font-semibold  ring-1 ring-inset ring-gray-300 focus:z-10`}
-              >
-                <span></span>Entrada
-              </button>
-              <button
-                onClick={() => setSei(true)}
-                type="button"
-                className={`relative -ml-px inline-flex items-center rounded-r-md py-2 px-4 ${sei ? 'bg-slate-600 text-white' : 'bg-white text-gray-900 hover:bg-gray-50'} text-sm font-semibold  ring-1 ring-inset ring-gray-300 focus:z-10`}
-              >
-                <span></span>Saída
-              </button>
-            </span>
-          </div>
           
       </Card>
     );
