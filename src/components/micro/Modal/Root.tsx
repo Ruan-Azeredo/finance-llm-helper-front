32838b9a742c642aefe33ed8f7fa53ba850486e9
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-19 16:00:46 -0300

    :bug: fix: remove overflow hidden

--- src/components/micro/Modal/Root.tsx
+++ src/components/micro/Modal/Root.tsx
@@ -19,11 +19,11 @@ const Root = (
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                         transition
-                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full md:max-w-3xl sm:max-w-xl data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                        className="relative transform rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full md:max-w-3xl sm:max-w-xl data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                     >
                         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
                         <div className="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0">
-                            <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:w-full sm:max-w-3xl sm:p-6">
+                            <div className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:w-full sm:max-w-3xl sm:p-6">
                                 {children}
                             </div>
                         </div>
