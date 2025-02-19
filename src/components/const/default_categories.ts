d80a11e656db7af5111ba0f38539be838fb5189d
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-19 16:52:29 -0300

    :truck: refactor: deffault categories

--- src/components/const/default_categories.ts
+++ src/components/const/default_categories.ts
@@ -44,29 +44,3 @@ export const default_categories = [
         'color': 0
     }
 ]
-
-export const colors = [
-    '#5D72D6',  
-    '#5FD797',
-    '#D66459',
-    '#D6B65A',  
-    '#BD51A7',
-    '#6DC157',  
-    '#56B1C1',  
-    '#CC8E5A',
-    '#8656C1',
-    '#CCCA5A'
-]
-
-export const colors2 = [
-    '#6364F2',  
-    '#F26A63',
-    '#F2D863',
-    '#E69545',  
-    '#63F299',
-    '#4B4BB8',  
-    '#B8514B',  
-    '#B8A44B',
-    '#AB6F33',
-    '#4BB875'
-]
