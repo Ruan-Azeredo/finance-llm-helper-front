478a9a858d551b24f664e4d8d6458c8ba774fddb
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-19 15:01:28 -0300

    :sparkles: feat: update default categories array and add colors array

--- src/default_categories.ts
+++ src/default_categories.ts
@@ -1,13 +1,72 @@
 export const default_categories = [
-    "Alimentação",
-    "Receitas",
-    "Saúde",
-    "Transporte",
-    "Mercado", 
-    "Educação",
-    "Compras",
-    "Investimento",
-    "Transferencia para terceiros", 
-    "Telefone",
-    "Moradia"
+    {
+        'name': 'Alimentação',
+        'color': 0
+    },
+    {
+        'name': 'Receitas',
+        'color': 1
+    },
+    {
+        'name': 'Saúde',
+        'color': 2
+    },
+    {
+        'name': 'Transporte',
+        'color': 3
+    },
+    {
+        'name': 'Mercado',
+        'color': 4
+    },
+    {
+        'name': 'Educação',
+        'color': 5
+    },
+    {
+        'name': 'Compras',
+        'color': 6
+    },
+    {
+        'name': 'Investimento',
+        'color': 7
+    },
+    {
+        'name': 'Transferencia para terceiros',
+        'color': 8
+    },
+    {
+        'name': 'Telefone',
+        'color': 9
+    },
+    {
+        'name': 'Moradia',
+        'color': 0
+    }
+]
+
+export const colors = [
+    '#5D72D6',  
+    '#5FD797',
+    '#D66459',
+    '#D6B65A',  
+    '#BD51A7',
+    '#6DC157',  
+    '#56B1C1',  
+    '#CC8E5A',
+    '#8656C1',
+    '#CCCA5A'
+]
+
+export const colors2 = [
+    '#6364F2',  
+    '#F26A63',
+    '#F2D863',
+    '#E69545',  
+    '#63F299',
+    '#4B4BB8',  
+    '#B8514B',  
+    '#B8A44B',
+    '#AB6F33',
+    '#4BB875'
 ]
