5a9dbc8bc1d9d1e533ae730ba67d391c94557123
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-28 14:13:34 -0300

    :sparkles: feat: add className in section container

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,10 +1,10 @@
-export default function SectionContainer({ children, title }: { children: React.ReactNode, title: string}) {
+export default function SectionContainer({ children, title, className }: { children: React.ReactNode, title: string, className?: string }) {
     return (
-        <section aria-labelledby={title}>
+        <section aria-labelledby={title} className={className}>
             <h2 id={title} className="sr-only">
             {title}
             </h2>
-            <div className="overflow-visible rounded-lg bg-white shadow">
+            <div className={`overflow-visible rounded-lg bg-white shadow`}>
             {children}
             </div>
         </section>
