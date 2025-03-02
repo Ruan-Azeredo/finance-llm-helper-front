f2db9c4c78a9b067f316c80046d1a01799a13d63
Ruan-M2 <ruanazeredo@gmail.com>
2025-03-01 21:36:02 -0300

    :construction: build: is athenticated

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,37 +1,56 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LayoutContainer from "./LayoutContainer";
 import AddTransactions from "./pages/AddTransactions";
 import Login from "./pages/Login";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "./contexts/Auth";
+import AllTransactions from "./pages/AllTransactions";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
 
 const RouterManager = () => {
 
-    const { isAuthenticated } = useContext(AuthContext)
+    const { isAuthenticated, instance } = useContext(AuthContext)
+    console.log(isAuthenticated)
 
     const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-        return isAuthenticated ? children : <Navigate to="/login" />;
+        if(isAuthenticated !== undefined && isAuthenticated === true) {
+            return children
+        } else if(isAuthenticated !== undefined && isAuthenticated === false) return <Navigate to="/login" />
     }
 
-    const AuthRoute = () => {
-        return !isAuthenticated ? <Outlet/> : <Navigate to="/" />;
+    const AuthRoute = ({ children }: { children: JSX.Element }) => {
+        if(isAuthenticated !== undefined && isAuthenticated === false) {
+            return children
+        } else if(isAuthenticated !== undefined && isAuthenticated === true) return <Navigate to="/" />;
     }
+
+    const protectedRoutes = [
+        { path: "/", element: <AddTransactions/> },
+        { path: "/transactions", element: <AllTransactions/> },
+        { path: "/dashboard", element: <Dashboard/> }
+    ];
+
+    useEffect(() => {
+        console.log('isAuthenticated ', isAuthenticated)
+        console.log('instance.defaults.headers.common["Authorization"] ', instance.defaults.headers.common["Authorization"])
+    }, [isAuthenticated])
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={
-                    <ProtectedRoute>
-                        <LayoutContainer/>
-                    </ProtectedRoute>
-                }>
-                    <Route path="/" element={<AddTransactions/>}/>
+                {/* Protected Routes inside LayoutContainer */}
+                <Route element={<LayoutContainer />}>
+                    {protectedRoutes.map(({ path, element }) => (
+                        <Route key={path} path={path} element={<ProtectedRoute>{element}</ProtectedRoute>} />
+                    ))}
                 </Route>
-                <Route element={
-                    <AuthRoute/>
-                }>
-                    <Route path="login" element={<Login/>}/>
-                </Route>
+
+                {/* Public Routes */}
+                <Route path="/login" element={<AuthRoute><Login /></AuthRoute>} />
+
+                {/* Catch-All Route for 404 */}
+                <Route path="*" element={<NotFound/>} />
             </Routes>
         </BrowserRouter>
     )
--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,32 +1,43 @@
 import axios, { AxiosInstance } from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 interface AuthContextType {
     instance: AxiosInstance
     signIn: (email: string, password: string) => void
     signOut: () => void
-    isAuthenticated: boolean
+    isAuthenticated: boolean | undefined
 }
 
 export const AuthContext = createContext<AuthContextType>({
     instance: axios,
     signIn: () => {},
     signOut: () => {},
-    isAuthenticated: false
+    isAuthenticated: undefined
 })
 
 export function AuthProvider({ children }: {children: React.ReactNode}) {
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState()
 
     const instance = axios.create({
         baseURL: 'http://127.0.0.1:8000',
-/*         headers: {
-            common: {
-                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJydWFuQGdtaWwuY29tIiwiZXhwIjoxNzQwNDA2MDE4fQ.VlhIdJncHlsboP1RNfzI9MoqqGEGa7IZEa2-T5uWth8`
-            }
-        } */
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        }
     })
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            console.log('token found: ', localStorage.getItem('token'))
+            setIsAuthenticated(true)
+            instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+            instance.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+            console.log('instance.defaults.headers.common["Authorization"] ', instance.defaults.headers.common['Authorization'])
+            console.log('instance.defaults.headers.co', instance.defaults.headers.post['Access-Control-Allow-Origin'])
+        } else {
+            setIsAuthenticated(false)
+        }
+    }, [instance])
 
     async function signIn(email: string, password: string) {
         try {
